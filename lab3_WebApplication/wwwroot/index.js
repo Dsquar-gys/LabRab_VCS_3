2c81f7ad662f905ab70d44313cfcf902fd10433a
daniilmiagkov <59875675+daniilmiagkov@users.noreply.github.com>
2023-04-16 22:40:59 +0300

    Сделана функция для построения двух графиков

--- lab3_WebApplication/wwwroot/index.js
+++ lab3_WebApplication/wwwroot/index.js
@@ -1,26 +1,3 @@
-
-const ctx = document.getElementById('myChart');
-
-const chart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: [],
-    datasets: [{
-      label: 'Статистика за последние 15 лет',
-      borderWidth: 1,
-      data: [],
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    },
-    responsive: true
-  }
-});
-
 document.getElementById('dmitrichenko').addEventListener('click', () => {
   get('d');
 })
@@ -50,9 +27,16 @@ async function get(type) {
       case ('d'):
         D(data);
         break;
+      case ('z'):
+        Z(data);
+        break;
+      case ('m'):
+        M(data);
+        break;
+      case ('V'):
+        V(data);
+        break;
     }
-
-    chart.update()
   }
   else {
     // если произошла ошибка, получаем сообщение об ошибке
@@ -62,25 +46,91 @@ async function get(type) {
 }
 
 function D(data) {
-  chart.data.datasets.push({
-    borderWidth: 1,
-    data: [],
-  })
+  const doc = document.getElementById('graph')
+  doc.innerHTML = ''
+/*  const c1 = document.createElement('canvas')*/
+  doc.innerHTML += '<canvas id="myChart1"></canvas>'
+  doc.innerHTML += '<canvas id="myChart2" ></canvas>'
+  
+  const ctx1 = document.getElementById('myChart1');
+  const chart1 = new Chart(ctx1, {type: 'line', data: {labels: [], datasets: [{label: '', borderWidth: 1, data: [],}]}, options: {scales: {y: {beginAtZero: true}}},});
+  
+  const ctx2 = document.getElementById('myChart2');
+  const chart2 =  new Chart(ctx2, {
+    type: 'line',
+    data: {
+      labels: [],
+      datasets: [{
+        label: '',
+        data: [],
+        borderWidth: 1,
+        title: 'fas',
+
+        
+      
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        
+        }
+      },
+
+    }
+  });
+
   for (let i = 0; i < data.data.length; i++) {
-    chart.data.datasets[0].data[i] = data.data[i]
-    chart.data.datasets[1].data[i] = data.valueChanges[i]
+    chart1.data.datasets[0].data[i] = data.data[i]
+    chart2.data.datasets[0].data[i] = data.valueChanges[i]
   }
   
-  chart.data.labels = data.yearTable;
+  chart1.data.labels = data.yearTable;
+  chart2.data.labels = data.yearTable;
+  
+  chart1.update()
+  chart2.update()
+
+  table(data)
 }
 
-/*function Z(data) {
+function table(data) {
+  const grid = document.getElementById('output');
+  grid.innerHTML = ''
+  let div;
+  for (let i in data) {
+    div = document.createElement('div')
+    let j;
+    for (j of data[i]) {
+      div.innerHTML += `<p>${j}</p>`
+    }
+    grid.append(div);
+  }
+  
+}
+
+function Z(data) {
+  const doc = document.getElementById('graph')
+  doc.innerHTML = ''
+  doc.innerHTML += '<canvas id="myChart1"></canvas>'
+  
+  const ctx1 = document.getElementById('myChart1');
+  const chart1 = new Chart(ctx1, {
+    type: 'line',
+    data: {labels: [], datasets: [{label: '', borderWidth: 1, data: [],}]},
+    options: {scales: {y: {beginAtZero: true}}},
+  });
+  
+  
   for (let i = 0; i < data.data.length; i++) {
-    chart.data.datasets[0].data[i] = data.data[i]
+    chart1.data.datasets[0].data[i] = data.data[i]
   }
-  console.log(data);
-  for (let i = 0; i < data.valueChanges.length; i++) {
-    chart.data.datasets[0].data[i] = data.valueChanges[i]
-  }
-  chart.data.labels = data.yearTable;*/
+  
+  chart1.data.labels = data.yearTable;
+  
+  chart1.update()
+  
+  table(data)
+}
 
