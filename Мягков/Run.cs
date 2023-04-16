027ce7bd01b61286b1ec6d281fda5a08443f44c9
daniilmiagkov <59875675+daniilmiagkov@users.noreply.github.com>
2023-04-16 16:17:44 +0300

    Добавлены методы для работы с данными

--- Мягков/Run.cs
+++ Мягков/Run.cs
@@ -1,13 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace LabRab_3.Мягков
 {
     public class Run
     {
+        public List<Day> Days { get; set; }
+
+        public decimal Sum { get; set; }
+
+        public Run()
+        {
+            Days = new List<Day>();
+            Sum = 0;
+        }
+
+        public void CreateDays()
+        {
+            Random random = new Random();
+            DateTime today = DateTime.Now;
+            DateTime temp = today;
+            while (today.Day - 1 != temp.Day)
+            {
+                Days.Add(new Day(
+                    temp.Day,
+                    temp.ToString(),
+                    random.Next(9, 10) / 15,
+                    random.Next(1, 4) / 15,
+                    random.Next(5, 9) / 15,
+                    random.Next(9, 10) / 15,
+                    random.Next(1, 4) / 15,
+                    random.Next(30, 180)
+                    ));
+                temp = temp.AddDays(1);
+            }
+        }
+        public void SaveDays()
+        {
+
+            StreamWriter writer = new StreamWriter("Data.json", false);
+
+            writer.Write(JsonSerializer.Serialize<List<Day>>(Days));
+
+            writer.Close();
+
+        }
+
+        public void GetDays()
+        {
+            StreamReader reader = new StreamReader("Data.json");
+
+            string s = reader.ReadToEnd();
+
+            if (s != "")
+                Days = JsonSerializer.Deserialize<List<Day>>(s);
+
+            reader.Close();
+        }
+
+        public void Print()
+        {
+            foreach (Day day in Days)
+            {
+                Console.WriteLine(day.Date);
+            }
+        }
+
+        public void FindSum()
+        {
+            foreach (Day day in Days)
+            {
+                string s = DateTime.Parse(day.Date).ToString("ddd",
+                        new CultureInfo("ru-Ru"));
+                if (s == "Сб" || s == "Вс")
+                {
+                    Sum += day.Distance;
+                }
+            }
+
+        }
     }
 
     public class Day
