3946015f8c987bcd3f8f4550f8ffacbfdb3e3a07
daniilmiagkov <59875675+daniilmiagkov@users.noreply.github.com>
2023-04-16 16:15:46 +0300

    Добавлен класс для хранения данных о дне

--- Мягков/Run.cs
+++ Мягков/Run.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace LabRab_3.Мягков
 {
-    internal class Run
+    public class Run
     {
+    }
+
+    public class Day
+    {
+        public Day(
+            int number,
+            string date,
+            decimal maxSpeed,
+            decimal minSpeed,
+            decimal averageSpeed,
+            decimal averagePulse,
+            decimal distance,
+            int minutes)
+        {
+            Number = number;
+            Date = date;
+            MaxSpeed = maxSpeed;
+            MinSpeed = minSpeed;
+            AverageSpeed = averageSpeed;
+            AveragePulse = averagePulse;
+            Distance = distance;
+            Minutes = minutes;
+        }
+
+        public int Number { get; set; }
+        public string Date { get; set; }
+        public decimal MaxSpeed { get; set; }
+        public decimal MinSpeed { get; set; }
+        public decimal AverageSpeed { get; set; }
+        public decimal AveragePulse { get; set; }
+        public decimal Distance { get; set; }
+        public int Minutes { get; set; }
+
     }
 }
